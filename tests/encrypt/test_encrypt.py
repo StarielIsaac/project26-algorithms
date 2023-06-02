349cabf0bb87bc1bf500bbb32ac4442f912a47ab
Stariel Isaac <starielisaac@gmail.com>
2023-06-02 18:57:01 -0300

    feat: implements asserts in test_encrypt.py

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,4 +3,10 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as errorMessage:
+        encrypt_message(None, 5)
+    assert str(errorMessage.value) == "tipo inválido para message"
+
+    with pytest.raises(TypeError) as erroKey:
+        encrypt_message("testes", None)
+    assert str(erroKey.value) == "tipo inválido para key"
