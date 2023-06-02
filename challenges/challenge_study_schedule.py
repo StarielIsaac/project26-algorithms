f36391209abc74050809a8e6f17df302fcc8a728
Stariel Isaac <starielisaac@gmail.com>
2023-06-02 17:24:47 -0300

    refactor: refactored function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,20 +7,12 @@ def study_schedule(permanence_period, target_time):
     count = 0
 
     for tupple in permanence_period:
-        point1 = tupple[index]
-        point2 = tupple[index + 1]
-
         if not (type(tupple[index]) is int and type(tupple[index + 1]) is int):
             return None
 
-        valuesList = list(range(point1, point2 + 1))
+        valuesList = list(range(tupple[index], tupple[index + 1] + 1))
 
         if target_time in valuesList:
             count += 1
 
     return count
-    # print(valuesList)
-    # print("point1:", point1, "point2:", point2)
-
-
-# print(study_schedule([(2, 4), (4, 6), (6, 8), [2, 4]], 4))
