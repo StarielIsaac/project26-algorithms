325292a1c0d55ca3d97664cdaf413e4557c66ee9
Stariel Isaac <starielisaac@gmail.com>
2023-06-05 16:09:44 -0300

    feat: implemented function find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,21 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_Numbers(nums):
+        return False
+
+    if nums[0] in nums[1:]:
+        return nums[0]
+
+    return find_duplicate(nums[1:])
+
+
+def validate_Numbers(nums):
+    if not nums or type(nums) == str:
+        return False
+
+    if len(nums) == 1 or len(set(nums)) == len(nums):
+        return False
+
+    if nums[0] < 0:
+        return False
+
+    return True
