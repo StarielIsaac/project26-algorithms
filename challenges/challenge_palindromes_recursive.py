72baeae404b297d427967f7ced87668c34691d88
Stariel Isaac <starielisaac@gmail.com>
2023-06-05 12:10:04 -0300

    feat: implemented function challenge_palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # condição de parada
+    if low_index >= high_index:
+        return True
+
+    # se a primeira letra for igual a ultima, chama novamente a chamada
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
+
+
+print(is_palindrome_recursive('arara', 1, 3))
